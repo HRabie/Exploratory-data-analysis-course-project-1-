f8c372234b3aed0654209e12ff2ff36cac336db9
Hassan Rabie <Hassan.rabie@gmail.com>
2016-01-07 23:07:19 +0200

    Revert "Plot1"
    This reverts commit c0f3227053ab04c82ef8dc93c6ff7b1576513c53.

--- Plot1.R
+++
@@ -1,8 +0,0 @@
-data <- read.table("~/Data2.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-test=data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-ActivePower <- as.numeric(test$Global_active_power)
-
-png("Plot1.png", width=480, height=480)
-hist(ActivePower ,col="red", main="Global Active Power", xlab ="Global Active Power (kilowatts)")
-dev.off()
