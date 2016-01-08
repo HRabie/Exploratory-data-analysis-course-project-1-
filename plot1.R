1ea46b2fe0fe0c6ef4174fc5a5650a5f32b3c6e6
HRabie <hassan.rabie@gmail.com>
2016-01-08 22:32:11 +0200

    Create plot 1

--- plot1.R
+++ plot1.R
@@ -3,7 +3,6 @@
 #loading the dataset into R
 data <- read.table("~/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
-
 #Using data from the dates 2007-02-01 and 2007-02-02
 test=data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
